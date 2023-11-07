8a77f78e14cc1d62d370342c75a5a19c2ccd7eb4
Alyssa Tadeo <alyssajtadeo@gmail.com>
2023-11-07 15:26:22 -0800

    remove firebase creds

--- nosql/src/index.js
+++ nosql/src/index.js
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB823bF8rK7VT4f1Tu31X7mLvvOhPnhWBU",
-  authDomain: "tla-databases-workshop.firebaseapp.com",
-  projectId: "tla-databases-workshop",
-  storageBucket: "tla-databases-workshop.appspot.com",
-  messagingSenderId: "1094869792400",
-  appId: "1:1094869792400:web:20b075d6a3348c02c20a15"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 
