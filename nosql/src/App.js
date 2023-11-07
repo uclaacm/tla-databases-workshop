e47ed3ca2bacfe02f99469d92c546e73693f7e21
Alyssa Tadeo <alyssajtadeo@gmail.com>
2023-11-07 14:35:21 -0800

    add firebase imports for nosql demo

--- nosql/src/App.js
+++ nosql/src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs, addDoc, doc, deleteDoc } from 'firebase/firestore'
 import { db } from './index.js'
 import DeleteImage from './delete.svg'
 
